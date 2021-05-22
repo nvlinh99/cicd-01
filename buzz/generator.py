26bdbb75e00c0427107b8c12fa72a95b168937bb
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2021-05-22 10:30:21 +0700

    refactor

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,37 +1,10 @@
 from __future__ import print_function
 import random
 
-buzz = (
-	'continuous testing', 
-	'continuous integration',
-	'continuous deployment', 
-	'continuous improvement',
-	'devops'
-)
-
-adjectives = (
-	'complete',
-	'modern',
-	'self-service',
-	'integrated',
-	'end-to-end'
-)
-
-adverbs = (
-	'remarkably',
-	'enormously',
-	'substantially',
-	'significantly',
-	'seriously'
-)
-
-verbs = (
-	'accelerates',
-	'improves',
-	'enhances',
-	'revamps',
-	'boosts'
-)
+buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
+adjectives = ('complete', 'modern', 'self-service',	'integrated',	'end-to-end')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
+verbs = ('accelerates',	'improves', 'enhances', 'revamps', 'boosts')
 
 def sample(l, n = 1):
 		result = random.sample(l, n)
